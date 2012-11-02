b0ff7dbcbe369de1f368b35f96a58e3ab6d4346c
kc4271 <kc4271@gmail.com>
2012-11-02 14:59:23 +0800

    fix a bug, get full artist list

--- GetArtists.py
+++ GetArtists.py
@@ -4,16 +4,24 @@ import urllib
 import re
 import os
 
-def read_web():
-	_url = 'http://www.wikipaintings.org/en/alphabet'
-	print 'Loading...'
-	return  urllib.urlopen(_url).read()
+def read_web(alpha):
+	_url = 'http://www.wikipaintings.org/en/alphabet/' + alpha
+	return  urllib.urlopen(_url).read().decode('utf-8','ignore')
 
 if __name__ == "__main__":
-	page = read_web()
-	soup = BeautifulSoup(page)
-	seg = soup.findAll('h6', {'class':'artist-grouped b0'})
+	#page = read_web()
+	artists_list = []
+	begin = ord('a')
+	for i in range(26):
+		print 'Loading Page ' + chr(begin + i)
+		page = read_web(chr(begin + i))
+		soup = BeautifulSoup(page)
+		cates = soup.findAll('div',{'class':'search-item fLeft'})
+		hrefs = [item.findAll('a',{'href':re.compile('/en/.*')})[0]['href'] for item in cates]
+		artists_list.extend(hrefs)
+
+	
 	f = open('artists.txt','w')
-	for s in seg:
-		f.write(s.findAll('a')[0]['href'] + '\n')
+	for artist in artists_list:
+		f.write(u'%s\n' % artist)
 	f.close()
